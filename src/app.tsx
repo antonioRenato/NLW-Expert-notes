0540729ec20895e00a4f0c30f08f83d5d8e8cdc2
antonioRenato <antoniorpfs@gmail.com>
2024-12-15 20:45:07 -0300

    fixed bugs css

--- src/app.tsx
+++ src/app.tsx
@@ -25,24 +25,36 @@ export function App() {
           </p>
         </div>
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
           <span className="text-sm font-medium text-slate-300">
             há 2 dias
           </span>
           <p className="text-sm leading-6 text-slate-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aperiam? Reiciendis eius explicabo nobis hic eveniet esse itaque laudantium consectetur? Cumque dolores vitae reiciendis consequatur doloribus delectus blanditiis eveniet commodi.
           </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </div>
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
           <span className="text-sm font-medium text-slate-300">
             há 4 dias
           </span>
           <p className="text-sm leading-6 text-slate-400">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque officia libero in, nisi nihil iste eligendi? Repudiandae, ipsum dicta mollitia voluptates ex officia harum eos pariatur unde aliquid sequi aliquam.
           </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </div>
-       
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">
+            há 8 dias
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque officia libero in, nisi nihil iste eligendi? Repudiandae, ipsum dicta mollitia voluptates ex officia harum eos pariatur unde aliquid sequi aliquam.
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
       </div>     
     </div>    
   )
