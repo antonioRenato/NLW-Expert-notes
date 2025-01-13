f09fca1d6a6f004c222885cd7a754bfe0b46f6b3
antonioRenato <antoniorpfs@gmail.com>
2025-01-13 18:13:04 -0300

    create new card layout]

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Toaster } from 'sonner'
 import { App } from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster richColors />
   </StrictMode>,
 )
