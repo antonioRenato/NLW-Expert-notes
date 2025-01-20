fdedf6c7b91b5118ab3b63fdb2239cf32728a350
antonioRenato <antoniorpfs@gmail.com>
2025-01-20 20:11:09 -0300

    created new note card process

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard()
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({ onNoteCreated } : NewNoteCardProps)
 {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
   const [content, setContent] = useState('')
@@ -25,11 +29,13 @@ export function NewNoteCard()
 
   function handleSaveNote(event: FormEvent)
   {
-    event.preventDefault();
+    event.preventDefault()
 
-    console.log(content);
+    onNoteCreated(content)
+    setContent('')
+    setShouldShowOnBoarding(true)
 
-    toast.success('Nota criada com sucesso');
+    toast.success('Nota criada com sucesso')
   }
 
   return (
@@ -66,6 +72,7 @@ export function NewNoteCard()
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChanged}
+                  value={content}
                 />
               )}
             </div>
